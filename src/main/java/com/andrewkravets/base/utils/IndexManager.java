b8fb8d1afc4ac7676fa597ffdaa441b04fc9387f
Andrey Kravets <andrey.kravets@engagepoint.com>
2014-03-03 19:03:02 +0200

    IndexStorage.java  -addObject  -removeObject

--- src/main/java/com/andrewkravets/base/utils/IndexManager.java
+++ src/main/java/com/andrewkravets/base/utils/IndexManager.java
@@ -1,9 +1,15 @@
 package com.andrewkravets.base.utils;
 
+import com.google.gson.JsonObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * User: andrey.kravets
  * Date: 2/19/14 2:46 PM
  */
 public class IndexManager {
-
+    Map<JsonObject, List<Object>> objectsIndexes = new HashMap<JsonObject, List<Object>>();
 }
--- src/main/java/com/andrewkravets/base/utils/IndexStorage.java
+++ src/main/java/com/andrewkravets/base/utils/IndexStorage.java
@@ -1,8 +1,41 @@
 package com.andrewkravets.base.utils;
 
+import com.google.gson.JsonObject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * User: andrey.kravets
  * Date: 2/19/14 2:47 PM
  */
 public class IndexStorage {
+    Map<Object, List<JsonObject>> indexes = new ConcurrentHashMap<Object, List<JsonObject>>();
+
+    public void addObject(Object index, JsonObject object){
+        createIndexIfNotPresent(index);
+        indexes.get(index).add(object);
+    }
+
+    public void removeObject(Object index, JsonObject object){
+        createIndexIfNotPresent(index);
+        indexes.get(index).remove(object);
+    }
+
+    private boolean isIndexPresent(Object index){
+        return indexes.containsKey(index);
+    }
+
+    private void createNewIndex(Object index){
+        indexes.put(index, new CopyOnWriteArrayList<JsonObject>());
+    }
+
+    private void createIndexIfNotPresent(Object index){
+        if(!isIndexPresent(index)) createNewIndex(index);
+    }
+
+
+
 }
